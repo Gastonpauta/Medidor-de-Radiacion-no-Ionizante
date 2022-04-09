5a2b331e67002212cbdb8eb9871056cce3b99dd9
gastonpauta <gaston_pauta@hotmail.com>
2022-04-09 18:49:03 -0300

    nuevo

--- MR-2/MR-2.ino
+++ MR-2/MR-2.ino
@@ -1,9 +1,83 @@
-void setup() {
-  // put your setup code here, to run once:
+//YWROBOT
+//Compatible with the Arduino IDE 1.0
+//Library version:1.1
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+#include <SoftwareSerial.h>
+#include <TinyGPS.h>
 
+
+LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+float sensorValue = 0;
+TinyGPS gps;
+SoftwareSerial ss(12,13);//(RX,TX)
+
+void setup()
+{
+  lcd.init();                      // initialize the lcd 
+  lcd.backlight();
+  lcd.setCursor(0,0);
+  lcd.print("VALOR.");
+
+  //gps
+
+  Serial.begin(115200);
+  ss.begin(9600);
+  
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
 
+void loop()
+{
+    lcd.setCursor(0,8);
+   // read the analog in value:
+  sensorValue = analogRead(34);
+  lcd.print(sensorValue);
+  delay(200);
+
+  //gps
+  bool newData = false;
+  unsigned long chars;
+  unsigned short sentences, failed;
+
+  // For one second we parse GPS data and report some key values
+  for (unsigned long start = millis(); millis() - start < 1000;)
+  {
+    while (ss.available())
+    {
+      char c = ss.read();
+      Serial.write(c); // uncomment this line if you want to see the GPS data flowing
+      if (gps.encode(c)) // Did a new valid sentence come in?
+        newData = true;
+    }
+  }
+
+  if (newData)
+  {
+    float flat, flon;
+    unsigned long age;
+    gps.f_get_position(&flat, &flon, &age);
+    Serial.print("LAT=");
+    Serial.print(flat == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flat, 6);
+    Serial.print(" LON=");
+    Serial.print(flon == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flon, 6);
+    Serial.print(" SAT=");
+    Serial.print(gps.satellites() == TinyGPS::GPS_INVALID_SATELLITES ? 0 : gps.satellites());
+    Serial.print(" PREC=");
+    Serial.print(gps.hdop() == TinyGPS::GPS_INVALID_HDOP ? 0 : gps.hdop());
+    Serial.println();
+    return;
+  }
+  
+  gps.stats(&chars, &sentences, &failed);
+  /*Serial.print(" CHARS=");
+  Serial.print(chars);
+  Serial.print(" SENTENCES=");
+  Serial.print(sentences);
+  Serial.print(" CSUM ERR=");
+  Serial.println(failed);*/
+  if (chars == 0)
+    Serial.println("* No characters received from GPS: check wiring *");
+
+  
 }
